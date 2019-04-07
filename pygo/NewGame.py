2b56fc9dcf7632e44605b4dca231e5f12107ce33
pavel <pavel.kliska@gmail.com>
2019-04-07 16:57:06 +0200

    implemented PlayerSetupWidget (not styled )

--- pygo/NewGame.py
+++ pygo/NewGame.py
@@ -1,69 +1,148 @@
 from tkinter import *
 from tkinter.font import Font
 from PIL import Image, ImageTk
+from tkinter.colorchooser import askcolor
 
 from Config import config as cfg
 import Start
 
+class PlayerSetupWidget(Frame):
+
+	DEFAULT_COLORS = ['black', 'white', 'red', 'green', 'blue', 'violet']
+
+	class PlayerSettings(Frame):
+
+		def __init__(self, parent, color):
+			Frame.__init__(self, parent)
+			self.parent = parent
+			self.color = color
+			self.name_entry = Entry(self)
+			self.name_entry.pack(fill = BOTH, expand = True, side=LEFT)
+			self.color_button = Button(self, text=" ", bg=color,
+									   command=self.change_color)
+			self.color_button.pack(fill = Y, expand = False, side=LEFT)
+			self.remove_button = Button(self, text="-",
+										command=self.remove_player,
+										state=DISABLED)
+			self.remove_button.pack(fill = Y, expand = False, side=LEFT)
+
+		def remove_player(self):
+			self.parent.remove_entry(self)
+
+		def change_color(self):
+			self.color = askcolor(self.color)[1]
+			self.color_button.config(bg = self.color)
+
+		def get_data(self):
+			return (self.name_entry.get(), self.color)
+
+	def __init__(self, parent):
+		Frame.__init__(self, parent)
+		self.entries = []
+		self.add_player_button = Button(self, text="Add player",
+										command=self.add_entry)
+		self.add_player_button.pack(fill=X, side=TOP)
+		self.add_entry()
+		self.add_entry()
+
+	def remove_entry(self, e):
+		e.pack_forget()
+		self.entries.remove(e)
+
+		if len(self.entries) < 6 and not self.add_player_button.winfo_ismapped():
+			self.add_player_button.pack(fill=X, side=TOP)
+
+		if len(self.entries)==2:
+			for i in self.entries:
+				i.remove_button.config(state=DISABLED)
+
+	def add_entry(self):
+		self.add_player_button.pack_forget()
+		e = self.PlayerSettings(self, self.DEFAULT_COLORS[len(self.entries)])
+		e.pack(fill=X, side=TOP)
+		self.entries.append(e)
+		if len(self.entries) < 6:
+			self.add_player_button.pack(fill=X, side=TOP)
+
+		if len(self.entries) > 2:
+			for i in self.entries:
+				i.remove_button.config(state=NORMAL)
+
+	def get_data(self):
+		return [i.get_data() for i in entries]
+
+class NewGame(Frame):
+
+	def __init__(self, parent, controller):
+		Frame.__init__(self, parent)
+		self.controller = controller
+
+		self.configure(background=cfg.bg_color)
+
+
+		im = Image.open(cfg.home_icon)
+		ph = ImageTk.PhotoImage(im.resize((40, 40), Image.ANTIALIAS))
+
+		relh_home = relw_home = Start.getRelH(40, cfg.x_window_size, but=True)
+
+		home_button = Button(self, image=ph, activebackground='light salmon', bg=cfg.bg_color, fg='black', relief='flat', highlightthickness=5, highlightbackground='black')
+		home_button.place(anchor='center', relheight=relh_home/1.5, relwidth=relw_home/1.5, relx=0.05, rely=0.05)
+
+		game_query_str = 'Game name:'
+		dimension_str = 'Dimension:'
+
+		query_font = Font(family=cfg.font_family, size=20)
+
+		relh_query = Start.getRelH(20, cfg.y_window_size)
+		relw_query_str = Start.getRelW(game_query_str, 20, cfg.x_window_size)
+		relw_dim_str = Start.getRelW(dimension_str, 20, cfg.x_window_size)
+
+		game_query_lab = Label(self, text=game_query_str, font=query_font, anchor='center', foreground='black', background=cfg.bg_color)
+		game_query_lab.place(anchor='center', relheight=relh_query, relwidth=relw_query_str, relx=0.3, rely=0.2)
+
+		dimension_lab = Label(self, text=dimension_str, font=query_font, anchor='center', foreground='black', background=cfg.bg_color)
+		dimension_lab.place(anchor='center', relheight=relh_query, relwidth=relw_dim_str, relx=0.3, rely=0.28)
+
+		n_chars_name = 20
+		relw_game_name = Start.getRelW('a'*n_chars_name, 20, cfg.x_window_size)
+
+		game_name_entry = Entry(self, bg = cfg.bg_color, font=query_font, fg='black', relief='flat')
+		game_name_entry.place(anchor='center', relheight=relh_query, relwidth=relw_game_name, relx=0.57, rely=0.2)
+
+		dim_options = ['9x9', '13x13', '17x17', '19x19']
+		dim_select = StringVar(self)
+		dim_select.set(dim_options[-1])
+
+		relw_opt = Start.getRelW(dim_options[-1], 20, cfg.x_window_size)
+
+		option_menu = OptionMenu(self, dim_select, *dim_options)
+		option_menu.configure(activebackground=cfg.bg_color, activeforeground='black')
+		option_menu.configure(anchor='center')
+		option_menu.configure(background=cfg.bg_color, foreground='black')
+		option_menu.configure(font=query_font)
+		option_menu.configure(disabledforeground=cfg.bg_color)
+		option_menu.configure(highlightthickness=1)
+		option_menu.configure(relief='flat')
+		option_menu.place(anchor='center', relheight=relh_query, relwidth=relw_opt*1.5, relx=0.46, rely=0.28)
+		test = PlayerSetupWidget(self)
+		test.place(relx=0.5, rely=0.5)
+
+
+		start_game_str = 'Start game!'
+		relh_start_str = Start.getRelH(20, cfg.y_window_size, but=True)
+		relw_start_str = Start.getRelW(start_game_str, 20, cfg.x_window_size)
+
+		start_game_but = Button(self, text=start_game_str, font=query_font, activebackground='pale green', bg=cfg.bg_color, fg='black', relief='flat', highlightthickness=5, highlightbackground='black')
+		start_game_but.place(anchor='center', relheight=relh_start_str, relwidth=relw_start_str, relx=0.8, rely=0.9)
+
 if __name__ == '__main__':
 
 	root = Tk()
-
-	im = Image.open(cfg.home_icon)
-	ph = ImageTk.PhotoImage(im.resize((40, 40), Image.ANTIALIAS))
-
-	relh_home = relw_home = Start.getRelH(40, cfg.x_window_size, but=True)
-
 	root.geometry('{}x{}'.format(cfg.x_window_size, cfg.y_window_size))
 	root.resizable(cfg.resize_window_x, cfg.resize_window_y)
-	root.configure(background=cfg.bg_color)
+	root.columnconfigure(0, weight=1)
+	root.rowconfigure(0, weight=1)
 	root.title('Py-Go')
-
-	home_button = Button(image=ph, activebackground='light salmon', bg=cfg.bg_color, fg='black', relief='flat', highlightthickness=5, highlightbackground='black')
-	home_button.place(anchor='center', relheight=relh_home/1.5, relwidth=relw_home/1.5, relx=0.05, rely=0.05)
-
-	game_query_str = 'Game name:'
-	dimension_str = 'Dimension:'
-
-	query_font = Font(family=cfg.font_family, size=20)
-
-	relh_query = Start.getRelH(20, cfg.y_window_size)
-	relw_query_str = Start.getRelW(game_query_str, 20, cfg.x_window_size)
-	relw_dim_str = Start.getRelW(dimension_str, 20, cfg.x_window_size)
-
-	game_query_lab = Label(root, text=game_query_str, font=query_font, anchor='center', foreground='black', background=cfg.bg_color)
-	game_query_lab.place(anchor='center', relheight=relh_query, relwidth=relw_query_str, relx=0.3, rely=0.2)
-
-	dimension_lab = Label(root, text=dimension_str, font=query_font, anchor='center', foreground='black', background=cfg.bg_color)
-	dimension_lab.place(anchor='center', relheight=relh_query, relwidth=relw_dim_str, relx=0.3, rely=0.28)
-
-	n_chars_name = 20
-	relw_game_name = Start.getRelW('a'*n_chars_name, 20, cfg.x_window_size)
-
-	game_name_entry = Entry(root, bg = cfg.bg_color, font=query_font, fg='black', relief='flat')
-	game_name_entry.place(anchor='center', relheight=relh_query, relwidth=relw_game_name, relx=0.57, rely=0.2)
-
-	dim_options = ['9x9', '13x13', '17x17', '19x19']
-	dim_select = StringVar(root)
-	dim_select.set(dim_options[-1])
-
-	relw_opt = Start.getRelW(dim_options[-1], 20, cfg.x_window_size)
-
-	option_menu = OptionMenu(root, dim_select, *dim_options)
-	option_menu.configure(activebackground=cfg.bg_color, activeforeground='black')
-	option_menu.configure(anchor='center')
-	option_menu.configure(background=cfg.bg_color, foreground='black')
-	option_menu.configure(font=query_font)
-	option_menu.configure(disabledforeground=cfg.bg_color)
-	option_menu.configure(highlightthickness=1)
-	option_menu.configure(relief='flat')
-	option_menu.place(anchor='center', relheight=relh_query, relwidth=relw_opt*1.5, relx=0.46, rely=0.28)
-
-	start_game_str = 'Start game!'
-	relh_start_str = Start.getRelH(20, cfg.y_window_size, but=True)
-	relw_start_str = Start.getRelW(start_game_str, 20, cfg.x_window_size)
-
-	start_game_but = Button(text=start_game_str, font=query_font, activebackground='pale green', bg=cfg.bg_color, fg='black', relief='flat', highlightthickness=5, highlightbackground='black')
-	start_game_but.place(anchor='center', relheight=relh_start_str, relwidth=relw_start_str, relx=0.8, rely=0.9)
-
+	f = NewGame(root, None)
+	f.grid(row=0, column=0, sticky="nsew")
 	root.mainloop()
---
+++ pygo/game.py
@@ -0,0 +1,126 @@
+import pickle
+
+def loadGame(fname):
+    with open(fname, 'r') as file:
+        game = pickle.load(file)
+    return game
+
+def saveGame(game, fname):
+    with open(fname, 'w+') as file:
+        pickle.dump(game, file, protocol=pickle.HIGHEST_PROTOCOL)
+
+class Game:
+
+    def __init__(self, dim, n_player):
+        self.board = [[None]*dim for i in range(dim)]
+        self.ko_protected = [None]*n_player
+        self.current_player = 0
+        self.dimension = dim
+        self.n_player = n_player
+
+    def in_bounds(self, row, col):
+        """Returns True if row and column are within the board"""
+        return 0<=row<self.dimension and 0<=col<self.dimension
+
+    def place_stone_as(self, row, col, player):
+        """Places a stone on row and column as a player. Automatically clears
+        all groups that are killed by the newly placed stone. If placing the
+        stone would be a suicide, or a violation of ko rule a ValueError is
+        raised.
+        """
+        if not self.in_bounds(row, col):
+            raise ValueError("Stone is outside of the board")
+        if self.board[row][col] is not None:
+            raise ValueError("There is already a stone in this location")
+
+        self.board[row][col]=player
+        if self.is_group_dead(row, col):
+            suicide=True
+            for i in self.neighbours(row, col):
+                if self.board[i[0]][i[1]] != player and self.is_group_dead(*i):
+                    suicide=False
+            if suicide:
+                self.board[row][col]=None
+                raise ValueError("A player can't commit suicide")
+
+        to_clear = []
+        for i in self.neighbours(row, col):
+            if self.is_group_dead(*i):
+                to_clear.append(i)
+
+        if (self.n_player == 2 and
+            len(to_clear) == 1 and len(list(self.group(*to_clear[0]))) == 1):
+            if to_clear[0] in self.ko_protected:
+                self.board[row][col] = None
+                raise ValueError("Ko rule violated")
+            else:
+                self.ko_protected[player]=(row, col)
+        for i in to_clear:
+            self.clear_group(*i)
+
+    def play_stone(self, row, col):
+        """Shortcut for placing a stone as current_player and changing the
+        current_player to the player who moves next
+        """
+        if self.ko_protected[self.current_player] is not None:
+            self.ko_protected[self.current_player] = None
+        self.place_stone_as(row, col, self.current_player)
+        self.current_player=(self.current_player+1)%self.n_player
+
+    def neighbours(self, row, col):
+        """Iterator over all the neighbouring stones to stone at row, col."""
+        if not self.in_bounds(row, col):
+            raise ValueError("Position is outside of the board")
+        dx = (-1, 0, 0, 1)
+        dy = (0, -1, 1, 0)
+        for i, j in zip(dx, dy):
+            if self.in_bounds(row + i, col + j):
+                yield (row + i, col + j)
+
+
+    def group(self, row, col):
+        """Iterator over all the stones that belong to the same group as the
+        stone at (row, col), yields tuples of coordinates (row, col)
+        """
+        if not self.in_bounds(row, col):
+            raise ValueError("Position is outside of the board")
+        visited = set()
+        to_visit = [(row, col)]
+        while len(to_visit)>0:
+            c = to_visit.pop()
+            visited.add(c)
+            yield c
+            for i in self.neighbours(c[0], c[1]):
+                if i not in visited and self.board[i[0]][i[1]]==self.board[row][col]:
+                    to_visit.append(i)
+
+    def clear_group(self, row, col):
+        """Removes a group from the board replacing it with empty spaces"""
+        for i in self.group(row, col):
+            self.board[i[0]][i[1]]=None
+
+    def is_group_dead(self, row, col):
+        """Returns True if group at row, col is dead i.e. it doesn't have any
+        free spaces around itself"""
+        if not self.in_bounds(row, col):
+            raise ValueError("Position is outside of the board")
+        if self.board[row][col] is None:
+            return False
+
+        for i in self.group(row, col):
+            for j in self.neighbours(*i):
+                if self.board[j[0]][j[1]] is None:
+                    return False
+        return True
+
+    def __str__(self):
+        r = "A {0}x{0} game with {1} players. Player {2} plays next \n".format(
+                            self.dimension, self.n_player, self.current_player)
+        for i in self.board:
+            for j in i:
+                if j is None:
+                    r+='.'
+                else:
+                    r+=chr(ord('0')+j)
+            r+='\n'
+        return r
