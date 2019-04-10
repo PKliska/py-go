208553f9dd4d9285bc4c0b34d634df1f3b624358
Patrik Kukic <patrikkukic@gmail.com>
2019-04-10 17:39:22 +0200

    reformatting

--- pygo/Start.py
+++ pygo/Start.py
@@ -6,93 +6,80 @@ import Utils as tls
 
 
 class Start(Frame):
-    def __init__(self, master):
-        Frame.__init__(self, master)
-        self.configure(background=cfg.bg_color)
 
-        title_str = "Py-Go"
-        desc_str = "An N-player Go implementation in Python using TKinter"
-        new_game_str = "New game"
-        saved_games_str = "Saved games"
+	def __init__(self, master):
+		Frame.__init__(self, master)
+		self.configure(background=cfg.bg_color)
 
-        relw_title = tls.getRelW(title_str, cfg.title_font_size, cfg.x_window_size)
-        relw_desc = tls.getRelW(desc_str, cfg.desc_font_size, cfg.x_window_size)
-        relw_new = tls.getRelW(
-            new_game_str, cfg.start_font_size, cfg.x_window_size, button=True
-        )
-        relw_saved = tls.getRelW(
-            saved_games_str, cfg.start_font_size, cfg.x_window_size, button=True
-        )
+		title_str = "Py-Go"
+		desc_str = "An N-player Go implementation in Python using TKinter"
+		new_game_str = "New game"
+		saved_games_str = "Saved games"
 
-        relh_title = tls.getRelH(cfg.title_font_size, cfg.y_window_size)
-        relh_desc = tls.getRelH(cfg.desc_font_size, cfg.y_window_size)
-        relh_new = tls.getRelH(cfg.start_font_size, cfg.y_window_size, button=True)
-        relh_saved = tls.getRelH(cfg.start_font_size, cfg.y_window_size, button=True)
-        title_labl = Label(
-            self,
-            text=title_str,
-            font=tls.crFont("title"),
-            foreground=cfg.fg_color,
-            background=cfg.bg_color,
-        )
-        title_labl.place(
-            anchor="center",
-            relheight=relh_title,
-            relwidth=relw_title,
-            relx=0.5,
-            rely=0.2,
-        )
+		relw_title = tls.getRelW(title_str, cfg.title_font_size, cfg.x_window_size)
+		relw_desc = tls.getRelW(desc_str, cfg.desc_font_size, cfg.x_window_size)
+		relw_new = tls.getRelW(
+		    new_game_str, cfg.start_font_size, cfg.x_window_size, button=True
+		)
+		relw_saved = tls.getRelW(
+		    saved_games_str, cfg.start_font_size, cfg.x_window_size, button=True
+		)
 
-        desc_labl = Label(
-            self,
-            text=desc_str,
-            font=tls.crFont("desc"),
-            foreground=cfg.fg_color,
-            background=cfg.bg_color,
-        )
-        desc_labl.place(
-            anchor="center",
-            relheight=relh_desc,
-            relwidth=relw_desc,
-            relx=0.5,
-            rely=0.35,
-        )
+		relh_title = tls.getRelH(cfg.title_font_size, cfg.y_window_size)
+		relh_desc = tls.getRelH(cfg.desc_font_size, cfg.y_window_size)
+		relh_new = tls.getRelH(cfg.start_font_size, cfg.y_window_size, button=True)
+		relh_saved = tls.getRelH(cfg.start_font_size, cfg.y_window_size, button=True)
+		title_labl = Label(
+		    self,
+		    text=title_str,
+		    font=tls.crFont("title"),
+		    foreground=cfg.fg_color,
+		    background=cfg.bg_color,
+		)
+		title_labl.place(
+		    anchor="center", relheight=relh_title, relwidth=relw_title, relx=0.5, rely=0.2
+		)
 
-        new_game_but = Button(
-            self,
-            text=new_game_str,
-            activebackground="pale green",
-            bg=cfg.bg_color,
-            font=tls.crFont("start"),
-            fg=cfg.fg_color,
-            relief=cfg.relief,
-            highlightthickness=cfg.border_thick,
-            highlightbackground=cfg.border_color,
-            command=lambda: self.master.switch_to(NewGame),
-        )
-        new_game_but.place(
-            anchor="center", relheight=relh_new, relwidth=relw_new, relx=0.75, rely=0.8
-        )
+		desc_labl = Label(
+		    self,
+		    text=desc_str,
+		    font=tls.crFont("desc"),
+		    foreground=cfg.fg_color,
+		    background=cfg.bg_color,
+		)
+		desc_labl.place(
+		    anchor="center", relheight=relh_desc, relwidth=relw_desc, relx=0.5, rely=0.35
+		)
 
-        saved_games_but = Button(
-            self,
-            text=saved_games_str,
-            activebackground="khaki",
-            bg=cfg.bg_color,
-            font=tls.crFont("start"),
-            fg=cfg.fg_color,
-            relief=cfg.relief,
-            highlightthickness=cfg.border_thick,
-            highlightbackground=cfg.border_color,
-            command=lambda: self.master.switch_to(SavedGames),
-        )
-        saved_games_but.place(
-            anchor="center",
-            relheight=relh_saved,
-            relwidth=relw_saved,
-            relx=0.25,
-            rely=0.8,
-        )
+		new_game_but = Button(
+			self,
+		    text=new_game_str,
+		    activebackground="pale green",
+		    bg=cfg.bg_color,
+		    font=tls.crFont("start"),
+		    fg=cfg.fg_color,
+		    relief=cfg.relief,
+		    highlightthickness=cfg.border_thick,
+		    highlightbackground=cfg.border_color,
+			command=lambda: self.master.switch_to(NewGame)
+		)
+		new_game_but.place(
+		    anchor="center", relheight=relh_new, relwidth=relw_new, relx=0.75, rely=0.8
+		)
 
-    def switch(self):
-        self.master.switch_to(NewGame)
+		saved_games_but = Button(
+			self,
+		    text=saved_games_str,
+		    activebackground="khaki",
+		    bg=cfg.bg_color,
+		    font=tls.crFont("start"),
+		    fg=cfg.fg_color,
+		    relief=cfg.relief,
+		    highlightthickness=cfg.border_thick,
+		    highlightbackground=cfg.border_color,
+			command=lambda: self.master.switch_to(SavedGames)
+		)
+		saved_games_but.place(
+		    anchor="center", relheight=relh_saved, relwidth=relw_saved, relx=0.25, rely=0.8
+		)
+		
